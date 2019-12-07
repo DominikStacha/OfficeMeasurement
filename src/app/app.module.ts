cc3bddb367c3b509ff16612aad949e44104abd0f
Dominik Stacha <dominik.stacha@sde.cz>
2019-12-07 19:46:16 +0100

    Reactive forms and Flex layout modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -12,10 +13,10 @@ import { AppComponent } from './app.component';
 import { ChartComponent } from './chart/chart.component';
 import { HomeComponent } from './home/home.component';
 import { MaterialModule } from './material/material.module';
+import { SensorDetailComponent } from './sensor-detail/sensor-detail.component';
 import { ChartDataService } from './shared/services/chart-data.service';
 import { MeasurementService } from './shared/services/measurement.service';
 import { SensorService } from './shared/services/sensor.service';
-import { SensorDetailComponent } from './sensor-detail/sensor-detail.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { SensorDetailComponent } from './sensor-detail/sensor-detail.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    ReactiveFormsModule,
     HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     ChartModule
