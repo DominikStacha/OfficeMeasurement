82dfc42abca534155294b35160027ee75ddb2963
Dominik Stacha <dominik.stacha@gmail.com>
2020-01-29 11:39:39 +0100

    Edit navigation items

--- src/app/shared/constants/navigation.constant.ts
+++ src/app/shared/constants/navigation.constant.ts
@@ -2,15 +2,11 @@ import { NavigationItem } from '../models/navigation-item.model';
 
 export const NavigationConstant: NavigationItem[] = [
   {
-    title: 'New Sensor',
-    path: '/new-sensor/'
-  },
-  {
     title: 'Home',
     path: '/'
   },
   {
-    title: 'About',
-    path: '/about/'
+    title: 'New Sensor',
+    path: '/new-sensor/'
   }
 ]
