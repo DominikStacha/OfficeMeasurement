9bc5425de4c96f46610822d9fb8cfed6b9625ed8
Dominik Stacha <dominik.stacha@sde.cz>
2020-01-30 22:27:38 +0100

    Sensor deatil udpate

--- src/app/shared/models/sensor-data.model.ts
+++ src/app/shared/models/sensor-data.model.ts
@@ -4,6 +4,9 @@ import { Sensor } from './sensor.model';
 export class SensorData {
   sensor: Sensor;
   lastHours: number;
+  lastTemperature: number | null;
+  lastHumidity: number | null;
+  lastAirPollution: number | null;
   temperatureData: ChartPoint[];
   humidityData: ChartPoint[];
   airPollutionData: ChartPoint[];
